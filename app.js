ca6682dd20ca6f437637bf4bc5ed6c850fffdd5e
Asadkhan Pathan <pathan.asad@gmail.com>
2023-01-03 21:16:10 +0530

    connect to mondodb

--- app.js
+++ app.js
@@ -3,12 +3,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
 
 const app = express();
+
+
+mongoose.set('strictQuery', true);
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+
 
 app.get("/", (req, res)=>{
     res.render("home");
