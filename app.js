6874feaef53df83465a55ce161551811aeeb6272
Asadkhan Pathan <pathan.asad@gmail.com>
2023-01-03 21:55:39 +0530

    Level - 1 Security of Username and Password

--- app.js
+++ app.js
@@ -16,6 +16,12 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
+const userSchema = new mongoose.Schema ({
+    email: String,
+    password: String
+})
+
+const User = new mongoose.model("User", userSchema);
 
 app.get("/", (req, res)=>{
     res.render("home");
@@ -27,6 +33,32 @@ app.get("/login", (req, res)=>{
 
 app.get("/register", (req, res)=>{
     res.render("register");
+})
+
+app.post("/register", (req, res)=>{
+    const newUser = new User ({
+        email: req.body.username,
+        password: req.body.password
+    })
+    newUser.save((err)=>{
+        if(err) {
+            console.log("Error : " + err);
+        } else {
+            res.render("secrets");
+        }
+    });
+})
+
+app.post("/login", (req, res)=>{
+    const userName = req.body.username;
+    const userPass = req.body.password;
+    User.findOne({email: userName}, (err, foundUser)=> {
+        if(foundUser.password == userPass) {
+            res.render("secrets");
+        } else {
+            res.redirect("/login")
+        }
+    })
 })
 
 app.listen(3000, ()=>{
